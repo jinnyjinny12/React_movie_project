a983e85b7e15a4aefcac68a050da808b38aba3cb
jinny <eujin.mstory@gmail.com>
2024-10-17 11:25:57 +0900

    build

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -7,7 +7,7 @@ function Movie ({id, coverImg, title, summary, genres}) {
         <img src={coverImg} alt="" />
         <h2>
 
-            <Link to={`/movie/${id}`}>{title}</Link>
+            <Link to={`${process.env.PUBLIC_URL}/movie/${id}`}>{title}</Link>
 
         </h2>
         <p>{summary}</p>
